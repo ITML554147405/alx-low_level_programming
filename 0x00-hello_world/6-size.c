9b47843663d585a484714b0bedc4277f34ffd663
Isaac Bonzo <isaacbonzo15@gmail.com>
2023-07-13 17:00:57 -0700

    Hello, printsf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Write a program that prints the size of various types of computers
- *  Return 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,9 +10,8 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
