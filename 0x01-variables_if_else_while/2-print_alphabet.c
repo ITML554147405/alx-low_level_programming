685ef81b1f3a4e6bc1dfc34ee882304093967928
Isaac Bonzo <isaacbonzo15@gmail.com>
2023-07-18 15:03:01 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a prgram that prints lowercase alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		printf("%c\n", ch);
+	}
+	return (0);
+}
