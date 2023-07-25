f535e767a770449091a0d875750441a55cd67af9
Isaac Bonzo <isaacbonzo15@gmail.com>
2023-07-25 00:42:07 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Lower and uppercase alphabets
+ *
+ * Return: Aways 0 (success)
+ */
+
+int main(void)
+{
+int lowercase = 'a';
+int uppercase = 'A';
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;		
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+		putchar('\n');
+	return (0);
+}
