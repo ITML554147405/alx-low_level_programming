dcf71f7084bd7cb5f14e88de45001ed5fe673c9a
Isaac Bonzo <isaacbonzo15@gmail.com>
2023-08-05 06:52:53 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Daylight School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "Me first world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
+}
